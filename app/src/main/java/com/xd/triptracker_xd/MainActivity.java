e4dfdde1e6975304dc13c71a508b48021100a3b2
Xavier Davis <xdavis@windows.linburg.com>
2019-04-25 10:14:44 -0400

    Initial commit

---
+++ app/src/main/java/com/xd/triptracker_xd/MainActivity.java
@@ -0,0 +1,36 @@
+package com.xd.triptracker_xd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText mEmailEditText;
+    EditText mPasswordEditText;
+    EditText mNameEditText;
+    Button mLoginButton;
+    TextView mSignUpTextView;
+    Button mSignUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mEmailEditText = (EditText)findViewById(R.id.enter_email);
+        mPasswordEditText = (EditText)findViewById(R.id.enter_password);
+        mNameEditText = (EditText)findViewById(R.id.enter_name);
+        mLoginButton = (Button)findViewById(R.id.login_button);
+        mSignUpTextView = (TextView)findViewById(R.id.sign_up_text);
+        mSignUpButton = (Button)findViewById(R.id.sign_up_button);
+
+
+        mSignUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSignUpTextView.getText() == getString(R.string.sign_up_button)
+            }
+        }
+    }
